f480d69fd05b0428076aa297b094f084110e922f
xuyanlong1991 <13331603398@163.com>
2019-12-23 18:47:11 +0800

    Test

---
+++ src/maopaohanshu.java
@@ -0,0 +1,23 @@
+public class maopaohanshu {
+    public static void main(String[] args) {
+        int arr[] = { 5, 10, 12, 6, 1 };
+        System.out.println("排序前数组为：");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("排序后组数为：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
